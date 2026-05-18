14726dc1b261aac85a8887eb0e34ce38c7594578
huynhcongtuan0978666620-alt <huynhcongtuan0978666620@gmail.com>
2026-05-18 22:33:48 +0700

    Update app_cua_ni.py

--- app_cua_ni.py
+++ app_cua_ni.py
@@ -20,7 +20,7 @@ import streamlit as st
 # =========================================================
 # 🎆 ĐOẠN 1: GIAO DIỆN CLONE ĐỒNG SIZE & HIỆU ỨNG PHÁO HOA V7
 # =========================================================
-     st.markdown("""
+st.markdown("""
 <style>
 /* 1. ẨN THÀNH PHẦN THỪA HỆ THỐNG */
 header, footer, .stAppDeployButton {
@@ -103,7 +103,8 @@ function createFirework(e) {
     }
 }
 </script>
-    """, unsafe_allow_html=True)
+""", unsafe_allow_html=True)
+
 
 
         /* TABS ĐỒNG BỘ */
