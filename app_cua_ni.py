cd1cfe89a3bd62f32355a2d4d8746e1e09178a3a
huynhcongtuan0978666620-alt <huynhcongtuan0978666620@gmail.com>
2026-05-18 15:39:43 +0700

    Update app_cua_ni.py

--- app_cua_ni.py
+++ app_cua_ni.py
@@ -421,5 +421,4 @@ def main():
                 with st.expander("🎫 Tạo/Xem mã nhân viên"):
                     try:
                         cl = get_gspread_client()
-                        # ĐÃ VÁ LỖI NGOẶC ĐƠN CHÍ MẠNG Ở DÒNG NÀY (BỔ SUNG DẤU ) CUỐI HÀM)
-    
+                        sh = cl.open_by_url(st.secrets["connections"]["gsheets"]["spreadsheet"]
