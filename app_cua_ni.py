080d88e988c27bd72142414ede4052837f13dbe5
huynhcongtuan0978666620-alt <huynhcongtuan0978666620@gmail.com>
2026-05-20 17:02:16 +0700

    Update app_cua_ni.py

--- app_cua_ni.py
+++ app_cua_ni.py
@@ -194,7 +194,7 @@ st.markdown("""
             background-color: #f1f3f5 !important; /* Màu nền xám nhạt cho tab chưa chọn */
             color: #495057 !important; /* Màu chữ xám tối */
             border-radius: 8px 8px 0 0 !important; /* Bo tròn nhẹ 2 góc trên của nút */
-            padding: 6px 6px !important;
+            padding: 2px 2px !important;
             margin-right: 4px !important;
             transition: all 0.3s ease !important;
         }
