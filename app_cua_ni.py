486c63b0a72b38f3c011c94ffca27e497c931cde
huynhcongtuan0978666620-alt <huynhcongtuan0978666620@gmail.com>
2026-05-26 18:45:45 +0700

    Update app_cua_ni.py

--- app_cua_ni.py
+++ app_cua_ni.py
@@ -114,7 +114,7 @@ def generate_css_animations():
         @keyframes fly-up-skywards-pure { 0% { transform: translateY(110vh); opacity: 0; } 10% { opacity: 0.6; } 90% { opacity: 0.6; } 100% { transform: translateY(-120vh); opacity: 0; } }
 
         st.markdown(CSS_TRÊN, unsafe_allow_html=True)
-<style>
+    <style>
     /* 1. Ẩn menu ba gạch của Streamlit ở góc trên bên phải */
     #MainMenu {visibility: hidden;}
 
@@ -129,7 +129,7 @@ def generate_css_animations():
     
     /* 5. Tùy chọn: Ẩn luôn thanh trạng thái của Streamlit (nếu cần) */
     div.stDeployButton {display:none;}
-</style>
+    </style>
 
     </style> 123
     """
