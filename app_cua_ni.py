991c8428b385c3898bb0134acb61e8bafb2c873d
huynhcongtuan0978666620-alt <huynhcongtuan0978666620@gmail.com>
2026-05-26 14:59:54 +0700

    Update app_cua_ni.py

--- app_cua_ni.py
+++ app_cua_ni.py
@@ -57,7 +57,7 @@ def generate_css_animations():
 
         .bang-hieu-lktv {
             display: flex !important; align-items: center !important; justify-content: flex-start !important; gap: 15px !important;
-            margin-bottom: 20px !important; padding: 12px 1px !important; border-radius: 8px !important; background: #ffffff !important; 
+            margin-bottom: 20px !important; padding: 12px 10px !important; border-radius: 8px !important; background: #ffffff !important; 
             box-shadow: 0px 2px 12px rgba(0,0,0,0.03) !important; border: 1px solid #f0f0f0 !important;
         }
         .logo-img { width: 55px !important; height: 55px !important; object-fit: cover !important; border-radius: 50% !important; border: 1px solid #eaeaea !important; margin: 0 !important; display: block !important; }
