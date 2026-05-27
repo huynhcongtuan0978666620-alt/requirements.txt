b700e8cd27c3c4bb833843043d0ee754c267b0a3
huynhcongtuan0978666620-alt <huynhcongtuan0978666620@gmail.com>
2026-05-27 18:42:26 +0700

    Update app_cua_ni.py

--- app_cua_ni.py
+++ app_cua_ni.py
@@ -365,7 +365,7 @@ def main():
     st.markdown(generate_css_animations(st.session_state.theme, st.session_state.get("role")), unsafe_allow_html=True)
     inject_advanced_ui_js()
     
-st.markdown("""<div class="banner-top">QUẢN LÝ DỊCH VỤ</div><div class="banner-bottom">SALON KIM HIỀN © 2026 - VISION V12</div>""", unsafe_allow_html=True)
+    st.markdown("""<div class="banner-top">QUẢN LÝ DỊCH VỤ</div><div class="banner-bottom">SALON KIM HIỀN © 2026 - VISION V12</div>""", unsafe_allow_html=True)
 
 
     settings = get_settings()
