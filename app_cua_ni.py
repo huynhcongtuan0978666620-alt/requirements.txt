559a9294d4c9e4ebb316a02d6003cb2ffbda3462
Tuấn Kiệt Huỳnh <huynhcongtuan0978666620@gmail.com>
2026-05-28 18:25:22 +0700

    Update app_cua_ni.py

--- app_cua_ni.py
+++ app_cua_ni.py
@@ -71,8 +71,8 @@ def inject_advanced_ui_js():
 
 def apply_v14_theme():
     # Tông màu theo Brand Identity
-    primary = "#EEE6DF"
-    bg_app = "#708090"
+    primary = "#F8F3EE"
+    bg_app = "#EEE6DF"
     
     st.markdown(f"""
     <style>
