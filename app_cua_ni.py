b1095a4a1d75d137cfba9d635964d935aebc5b5b
Tuấn Kiệt Huỳnh <huynhcongtuan0978666620@gmail.com>
2026-05-29 22:19:20 +0700

    Update app_cua_ni.py

--- app_cua_ni.py
+++ app_cua_ni.py
@@ -36,7 +36,7 @@ THEME_COLORS = {
     # --- Màu chữ (Text) ---
     "text_main": "#000000",         # Màu chữ chính (đậm, dễ đọc)
     "text_secondary": "#000000",    # Màu chữ phụ (dùng cho mô tả ngắn)
-    "text_muted": "#ffffff",        # Màu chữ mờ (dùng cho thông tin bổ sung, không quan trọng)
+    "text_muted": "#000000",        # Màu chữ mờ (dùng cho thông tin bổ sung, không quan trọng)
     "text_title": "#ffffff",        # Màu chữ tiêu đề (tạo cảm giác chuyên nghiệp)
     "text_badge": "#000000",        # Màu chữ bên trong nhãn (badge)
 
