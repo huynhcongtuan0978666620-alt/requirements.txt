1119adc26aabcd5612077147bb2dd162ad72878a
Tuấn Kiệt Huỳnh <huynhcongtuan0978666620@gmail.com>
2026-05-29 22:11:57 +0700

    Update app_cua_ni.py

--- app_cua_ni.py
+++ app_cua_ni.py
@@ -20,7 +20,7 @@ from email.mime.multipart import MIMEMultipart
 THEME_COLORS = {
     # --- Màu nền (Backgrounds) ---
     "bg_app": "#ffffff",            # Nền tổng thể của ứng dụng
-    "bg_card": "#32c739",           # Nền thẻ (card) hiển thị nội dung
+    "bg_card": "#e6d6aa",           # Nền thẻ (card) hiển thị nội dung
     "bg_box_chung": "#f8f9fa",      # Nền cho các khung chứa thông tin chung
     "bg_vip_box": "#eaf2e6",        # Nền cho khung thông tin VIP (màu xanh lá nhạt)
     "bg_shake_box": "#ffffff",      # Nền cho khung tính năng "lắc" hoặc sự kiện đặc biệt
