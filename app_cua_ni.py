d9fcf87994c15392f636d8be98cc7897c74ec146
Tuấn Kiệt Huỳnh <huynhcongtuan0978666620@gmail.com>
2026-05-29 21:34:44 +0700

    Update app_cua_ni.py

--- app_cua_ni.py
+++ app_cua_ni.py
@@ -59,7 +59,7 @@ def set_app_background(colors):
     gradient_css = f"""
     <style>
     .stApp {{
-        background: linear-gradient(to bottom, {colors['bg_app']}, #ffffff);
+        background: linear-gradient(to bottom, {colors['bg_app']}, #1161ed);
         background-attachment: fixed;
     }}
     </style>
