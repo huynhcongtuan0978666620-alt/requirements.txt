8bdf4fa9140625f723180e7cfe20615a830a0952
Tuấn Kiệt Huỳnh <huynhcongtuan0978666620@gmail.com>
2026-05-31 23:15:53 +0700

    Update app_cua_ni.py

--- app_cua_ni.py
+++ app_cua_ni.py
@@ -333,7 +333,7 @@ ten_dang_nhap = st.session_state.get("full_name", "Quý khách")
 marquee_code = f"""
 <div class="custom-marquee">
     <marquee scrollamount="4">
-        CHÀO MỪNG {ten_dang_nhap} ĐẾN VỚI SALON KIM HIỀN! CHÚC MỘT NGÀY BÃO ĐƠN VÀ CHỐT THẬT NHIỀU BILL NHA!
+        CHÀO MỪNG {ten_dang_nhap} ĐẾN VỚI SALON KIM HIỀN! CHÚC {ten_dang_nhap} MỘT LÀM VIỆC ĐẦY NĂNG LƯƠNG VÀ CHỐT THẬT NHIỀU BILL NHA !!!!!!!
     </marquee>
 </div>
 """
