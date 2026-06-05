ccdacb2dead2c072008fa58c176aba0663ff6e2e
Tuấn Kiệt Huỳnh <huynhcongtuan0978666620@gmail.com>
2026-06-05 15:53:36 +0700

    Update app_cua_ni.py

--- app_cua_ni.py
+++ app_cua_ni.py
@@ -22,7 +22,7 @@ from config import *
 # 4. LUỒNG ĐIỀU HƯỚNG VÀ XỬ LÝ CHÍNH
 # =====================================================================
 def main():
-    config.set_app_background(st, THEME_COLORS)
+config.set_app_background(st, THEME_COLORS)
 
     init_states = {
         "last_submit": None,
