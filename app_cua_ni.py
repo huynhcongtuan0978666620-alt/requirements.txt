cddc00c85151834ea581e54475d231857fef34cb
Tuấn Kiệt Huỳnh <huynhcongtuan0978666620@gmail.com>
2026-06-05 15:50:57 +0700

    Update app_cua_ni.py

--- app_cua_ni.py
+++ app_cua_ni.py
@@ -15,13 +15,12 @@ from email.mime.multipart import MIMEMultipart
 import streamlit.components.v1 as components
 from config import *
 
-
+# 1. Gọi các thiết lập Theme (Ní đã làm rồi)
+    set_app_background(st, THEME_COLORS)
 # =====================================================================
 # 4. LUỒNG ĐIỀU HƯỚNG VÀ XỬ LÝ CHÍNH
 # =====================================================================
 def main():
-    # 1. Gọi các thiết lập Theme (Ní đã làm rồi)
-    set_app_background(st, THEME_COLORS)
 
     init_states = {
         "last_submit": None,
