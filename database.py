aa005fa3e75c8dcdab46388443d3d6f8d2fda8ab
Tuấn Kiệt Huỳnh <huynhcongtuan0978666620@gmail.com>
2026-06-06 17:32:15 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,16 @@
-database.py
+# database.py
+import gspread
+from google.oauth2.service_account import Credentials
+import pandas as pd
+import streamlit as st
+
+def get_client():
+    # Ní bỏ đoạn code gspread.service_account(...) của ní vào đây
+    # Nên lưu credentials vào st.secrets để bảo mật ní nhé!
+    return gspread.service_account(filename="credentials.json")
+
+def get_data_from_sheet(sheet_name):
+    client = get_client()
+    sheet = client.open(config.TEN_FILE_SHEET).worksheet(sheet_name)
+    return pd.DataFrame(sheet.get_all_records())
+  
