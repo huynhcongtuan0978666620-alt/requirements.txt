ec4982656f73c7665e93fb648d52cc0a6e8e6613
Tuấn Kiệt Huỳnh <huynhcongtuan0978666620@gmail.com>
2026-06-08 00:18:31 +0700

    Update database.py

--- database.py
+++ database.py
@@ -7,15 +7,7 @@ from google.oauth2.service_account import Credentials
 @st.cache_resource
 def get_db_connection():
     # Ní dùng đúng cách ní đang dùng trong file chính để kết nối
-    creds = Credentials.from_service_account_info(
-        st.secrets["gcp_service_account"],
-        scopes=["https://www.googleapis.com/auth/spreadsheets"],
-    )
-    client = gspread.authorize(creds)
-    # Ní nhớ điền đúng tên file Google Sheet của ní ở đây
-    return client.open("Bản sao của BC_DULIEU_DEMO_2026").sheet1
-
-        # ... (đoạn trên ní giữ nguyên)
+            # ... (đoạn trên ní giữ nguyên)
 
         scope = [
             "https://www.googleapis.com/auth/spreadsheets",
@@ -28,4 +20,4 @@ def get_db_connection():
         
         url = secrets.get("spreadsheet", "")
         # ... (đoạn dưới ní giữ nguyên)
-
+    
