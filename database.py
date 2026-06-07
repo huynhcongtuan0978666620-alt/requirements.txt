dddb3cafc5493d1733fcb3ee5c3574ac8a961e61
Tuấn Kiệt Huỳnh <huynhcongtuan0978666620@gmail.com>
2026-06-08 01:18:15 +0700

    Update database.py

--- database.py
+++ database.py
@@ -38,7 +38,7 @@ def get_db_connection():
         if not url and "spreadsheet" in st.secrets:
             url = st.secrets["spreadsheet"]
             # Ní thay "Trang1" bằng tên Sheet chính xác trong file của ní, hoặc dùng .sheet1 để lấy trang đầu tiên
-        return (gc.open_by_key(url) if len(url) < 50 else gc.open_by_url(url)).sheet1
+        return (gc.open_by_key(url) if len(url) < 50 else gc.open_by_url(url)).NhanVien
 
         # return gc.open_by_key(url) if len(url) < 50 else gc.open_by_url(url)
     except Exception as e:
