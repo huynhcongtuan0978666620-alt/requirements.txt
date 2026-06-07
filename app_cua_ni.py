bebc28ed18b8073aaaeb33f48b5521129bbbb923
Tuấn Kiệt Huỳnh <huynhcongtuan0978666620@gmail.com>
2026-06-07 17:44:34 +0700

    Update app_cua_ni.py

--- app_cua_ni.py
+++ app_cua_ni.py
@@ -1638,10 +1638,10 @@ def main():
                             st.session_state.bill_vua_in = f"""<style>.hoa-don-khung table, .hoa-don-khung tr, .hoa-don-khung td {{border: none !important; background: transparent !important;}}</style>
 <div class="hoa-don-khung">
 <div style="text-align: center; border-bottom: 1px dashed {Config.THEME_COLORS['border_input']}; padding-bottom: 15px; margin-bottom: 20px;">
-    <div style="font-size: 27px; font-weight: 900; color: {Config.THEME_COLORS['text_main']};">{settings.get('TenTiem', 'SALON KIM HIỀN')}</div>
-    <div style="font-size: 13px; color: {Config.THEME_COLORS['text_secondary']}; margin-top:4px;">{settings.get('Diachi', '')}</div>
-    <div style="font-size: 13px; color: {Config.THEME_COLORS['text_secondary']};">SĐT: {settings.get('SDT', '')}</div>
-    <div style="font-size: 18px; font-weight: 800; color: {Config.THEME_COLORS['text_main']}; margin-top:10px;">HÓA ĐƠN DỊCH VỤ</div>
+    <div style="font-size: 22px; font-weight: 900; color: {Config.THEME_COLORS['text_main']};">{settings.get('TenTiem', 'SALON KIM HIỀN')}</div>
+    <div style="font-size: 12px; color: {Config.THEME_COLORS['text_secondary']}; margin-top:4px;">{settings.get('Diachi', '')}</div>
+    <div style="font-size: 12px; color: {Config.THEME_COLORS['text_secondary']};">SĐT: {settings.get('SDT', '')}</div>
+    <div style="font-size: 16px; font-weight: 800; color: {Config.THEME_COLORS['text_main']}; margin-top:10px;">HÓA ĐƠN DỊCH VỤ</div>
     <div style="font-size: 12px; color: {Config.THEME_COLORS['text_muted']}; margin-top:5px;">Mã số: {ma_hd}</div>
     <div style="font-size: 12px; color: {Config.THEME_COLORS['text_muted']}; margin-top:2px;">Số ĐH trong ngày: {count_orders_today()}</div>
 </div>
