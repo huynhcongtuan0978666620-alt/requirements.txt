aa1aadc21630de980877fddb64d803cf3c0ec436
Tuấn Kiệt Huỳnh <huynhcongtuan0978666620@gmail.com>
2026-06-14 16:56:46 +0700

    Update app_cua_ni.py

--- app_cua_ni.py
+++ app_cua_ni.py
@@ -2164,7 +2164,7 @@ def main():
                             if not df_today.empty:
                                 try:
                                     # Ní thay TÊN_HÀM_LẤY_SHEET_CỦA_NÍ bằng hàm thực tế trong App của ní
-                                    raw_data_dm = TÊN_HÀM_LẤY_SHEET_CỦA_NÍ("DanhMuc")
+                                    raw_data_dm = sh.worksheet("DanhMuc").get_all_values()
                                     df_dm = pd.DataFrame(
                                         raw_data_dm[1:], columns=raw_data_dm[0]
                                     )
